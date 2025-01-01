1b656fdd54ce6b28d6c7dab464ac6a5354b71bb4
Arthur Segato <arthursegato@protonmail.com>
2025-01-01 18:26:01 +0000

    Backup

---
+++ app/composables/poem.ts
@@ -0,0 +1,3 @@
+export const usePoem = () => {
+    return useState<string>("poem", () => "");
+};
---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const name = useName();
+
+const poem = usePoem();
+</script>
+
+<template>
+    <main class="w-full min-h-screen bg-[#F2F2F2] text-[#333333] font-['Open_Sans'] flex items-center justify-center">
+        <section class="w-fit flex flex-col gap-4 items-center" v-if="poem.length == 0">
+            <HomeTitle />
+            <LazyHomeForm />
+        </section>
+        <section v-else>
+        </section>
+    </main>
+</template>
